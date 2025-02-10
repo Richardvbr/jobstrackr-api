4a691cac78ced39c3f927ceb6b27dd4a0c2cfb12
Richard van Brunschot <richardvanbrunschot@gmail.com>
2025-02-10 21:15:52 +0100

    rm success:true

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -5,7 +5,7 @@ export async function getUsers(req: any, res: Response, next: NextFunction) {
   try {
     const users = await UserService.getUsers();
 
-    res.status(200).json({ success: true, data: users });
+    res.status(200).json({ data: users });
   } catch (error) {
     next(error);
   }
@@ -15,7 +15,7 @@ export async function getUser(req: any, res: Response, next: NextFunction) {
   try {
     const user = await UserService.getUser(req);
 
-    res.status(200).json({ success: true, data: user });
+    res.status(200).json({ data: user });
   } catch (error) {
     next(error);
   }
